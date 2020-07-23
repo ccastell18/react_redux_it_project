64c53f45775c3e679a6aa8f83867a36a98b1a60b
Christopher Castellano <c.castellano18@gmail.com>
2020-07-23 14:41:48 -0500

    start modals

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, Fragment } from 'react';
 
 import SearchBar from './components/layouts/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layouts/AddBtn';
 
 import 'materialize-css/dist/css/materialize.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -17,6 +18,7 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
         <Logs />
       </div>
     </Fragment>
